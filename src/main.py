4a7ba91866c43b29e1a3f4bec111828eb3bedb37
Dmitry Abt <dmitry.abt@gmail.com>
2025-03-30 19:11:18 +0700

    добавлены дополнительные атрибуты

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ import numpy as np
 def main():
     # Генерация данных
     print("Генерация синтетических данных")
-    data_handler = DataHandler(n_samples=1500, n_features=3, n_classes=3)
+    data_handler = DataHandler(n_samples=1500, n_features=5, n_classes=3)
     X, y = data_handler.generate_synthetic_data()
 
     # Визуализация данных
     print("\nВизуализация данных до классификации")
-    feature_names = ['Амплитуда', 'Частота', 'Анизотропия']
+    feature_names = ['Амплитуда', 'Частота', 'Анизотропия', 'Атрибут 4', 'Атрибут 5']
     data_handler.plot_features(X, y, feature_names)
     
     # Разделение данных (60% обучение / 20% валидация / 20% тест)
